359494c82c560ed938a3f776ae521c53c699c9e2
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-05-06 22:28:04 -0500

    add: maps

--- maps.js
+++ maps.js
@@ -23,17 +23,77 @@ maps.push(`
   OXXXXXXXXX
 `);
 maps.push(`
+  O--------X
+  -XXXXXXX--
+  --XXXXXXX-
+  X-XXXXXXX-
+  X---------
+  X-XXX-XXX-
+  X--XX-XXX-
+  XXX--I----
+  XXXXXXXXXX
+  XXXXXXXXXX
+`);
+maps.push(`
+  XXXXXXXXXX
+  XX------XX
+  X--XXX-XXX
+  X-XXXX-XXX
+  X-XXXX-XXX
+  X-XXXXIXXX
+  X-XXXXXXXX
+  X----O-XXX
+  XXXXXXXXXX
+  XXXXXXXXXX
+`);
+maps.push(`
+  IXXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXOXXX
+  --XXXX-XXX
+  X--XXX-XXX
+  XX-----XXX
+  XXXXXXXXXX
+`);
+maps.push(`
   O--XXXXXXX
   X--XXXXXXX
   XX----XXXX
   X--XX-XXXX
   X-XXX--XXX
   X-XXXX-XXX
-  XX--XX--XX
+  X---XX--XX
   XX--XXX-XX
-  XXXX---IXX
+  XXX----IXX
   XXXXXXXXXX
-  `);
+`);
+maps.push(`
+  XXXXXXXXXX
+  XXXX------
+  XXXX-XXXX-
+  ---X-XXXX-
+  -X-----OX-
+  --XXXXXXX-
+  X---------
+  XXXXX-XXXX
+  XXXXX-XOXX
+  XXXXX---XX
+`);
+maps.push(`
+  XXXXXXXXXX
+  X---XXXXXX
+  X-X--XXXXX
+  --XX-XXXXX
+  -XXX---OXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  -------IXX
+  XXXXXXXXXX
+`);
 maps.push(`
   I-----XXXX
   XXXXX-XXXX
@@ -46,3 +106,32 @@ maps.push(`
   XX-----OXX
   XXXXXXXXXX
 `);
+maps.push(`
+  OXXXX-----
+  -XXXX-XXX-
+  -XXXX-XXX-
+  ------XXX-
+  XXXXXXXXX-
+  XXXXXXXX--
+  XXXXXXX--X
+  XXXXXX--XX
+  XXX----XXX
+  I---XXXXXX
+`);
+
+/* 
+
+maps.push(`
+  XXXXXXXXXX
+  XXXXXXXXXX
+  XXXXXXXXXX
+  XXXXXXXXXX
+  XXXXXXXXXX
+  XXXXXXXXXX
+  XXXXXXXXXX
+  XXXXXXXXXX
+  XXXXXXXXXX
+  XXXXXXXXXX
+`);
+
+ */
